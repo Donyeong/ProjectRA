52b8647497826e189f8ae2e22594593161116589
Donyeong <tksgml97@gmail.com>
2025-07-23 12:58:14 +0900

    no message

---
+++ ProjectRA/Assets/PlayerAnimController.cs
@@ -0,0 +1,38 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerAnimController : NetworkBehaviour
+{
+    public Animator animator;
+	private void Awake()
+	{
+		animator = GetComponentInChildren<Animator>();
+	}
+
+	public void SetMove(bool isMove)
+	{
+		CmdSetBool("bMove", isMove);
+	}
+
+	public void SetRun(bool isRun)
+	{
+		CmdSetBool("bRun", isRun);
+	}
+
+	[Command]
+	public void CmdSetBool(string key, bool state)
+	{
+		animator.SetBool(key, state);
+		RpcSetBool(key, state);
+	}
+
+	[ClientRpc]
+	public void RpcSetBool(string key, bool state)
+	{
+		animator.SetBool(key, state);
+	}
+}
